110964ff5086fc473ffd8c4512ab3fe2fafeaf4e
Quốc Tuấn Vương <tuanvq.eledevo@gmail.com>
2024-07-11 10:21:03 +0000

    Dev tuan

---
+++ src/main/java/vn/eledevo/vksbe/entity/DeviceInfo.java
@@ -0,0 +1,34 @@
+package vn.eledevo.vksbe.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "device_info")
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class DeviceInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+    String deviceUuid;
+    @Column(columnDefinition = "varchar(255) default 'Not Connect'")
+    String status;
+    LocalDateTime createAt;
+    UUID createBy;
+    LocalDateTime updateAt;
+    UUID updateBy;
+    Boolean isDeleted;
+    @OneToMany(mappedBy = "deviceInfo", cascade = CascadeType.ALL, orphanRemoval = true)
+    List<UserDeviceInfoKey> userDeviceInfoKeys;
+}
