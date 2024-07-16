0311f204581c53230a6f079d50694853b5e48318
Quốc Tuấn Vương <tuanvq.eledevo@gmail.com>
2024-07-16 03:22:02 +0000

    Fix/entity device info

--- src/main/java/vn/eledevo/vksbe/entity/DeviceInfo.java
+++ src/main/java/vn/eledevo/vksbe/entity/DeviceInfo.java
@@ -28,10 +28,12 @@ public class DeviceInfo {
     @Column(columnDefinition = "varchar(255) default 'Not Connect'")
     String status;
 
-    LocalDateTime createAt;
-    UUID createBy;
-    LocalDateTime updateAt;
-    UUID updateBy;
+    LocalDateTime createdAt;
+    UUID createdBy;
+    LocalDateTime updatedAt;
+    UUID updatedBy;
+
+    @Column(columnDefinition = "number default 0")
     Boolean isDeleted;
 
     @OneToMany(mappedBy = "deviceInfo", cascade = CascadeType.ALL, orphanRemoval = true)
