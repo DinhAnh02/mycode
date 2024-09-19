5c6ca01cd6eb7a5607d832f904c7faa52cd4bcd8
TuanLinh-1 <linhtt.eledevo@gmail.com>
2024-09-19 10:10:51 +0700

    refactor(account):create_accountController_getAccountList_FixReview

--- src/main/java/vn/eledevo/vksbe/service/organization/OrganizationServiceImpl.java
+++ src/main/java/vn/eledevo/vksbe/service/organization/OrganizationServiceImpl.java
@@ -2,15 +2,19 @@ package vn.eledevo.vksbe.service.organization;
 
 import java.util.Optional;
 
+import org.springframework.stereotype.Service;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import vn.eledevo.vksbe.entity.Organizations;
 import vn.eledevo.vksbe.repository.OrganizationRepository;
 
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class OrganizationServiceImpl implements OrganizationService {
-    private final OrganizationRepository organizationRepository;
-
-    public OrganizationServiceImpl(OrganizationRepository organizationRepository) {
-        this.organizationRepository = organizationRepository;
-    }
+    OrganizationRepository organizationRepository;
 
     @Override
     public Boolean organizationNameChangeDetector(Long organizationId, String organizationName) {
