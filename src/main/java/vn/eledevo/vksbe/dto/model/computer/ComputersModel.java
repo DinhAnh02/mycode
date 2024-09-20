c342de46929d3dc7fbb336ffee954af8699345f4
Mạnh Nguyễn Tiến <manhnt.eledevo@gmail.com>
2024-09-20 04:46:52 +0000

    Fixbug/computer service update computer

--- src/main/java/vn/eledevo/vksbe/dto/model/computer/ComputersModel.java
+++ src/main/java/vn/eledevo/vksbe/dto/model/computer/ComputersModel.java
@@ -2,6 +2,8 @@ package vn.eledevo.vksbe.dto.model.computer;
 
 import java.time.LocalDateTime;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,11 +15,13 @@ import lombok.experimental.FieldDefaults;
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ComputersModel {
+    @NotBlank(message = "Tên máy tính không được để trống")
+    @Size(min = 10, message = "Tên máy tính phải có độ dài ít nhất từ 10 ký tự")
     String name;
-    String status;
+    @NotBlank(message = "Thương hiệu không được để trống")
     String brand;
+    @NotBlank
     String type;
+    @Size(max = 224, message = "Mô tả không vượt quá 225 kí tự")
     String note;
-    LocalDateTime updateAt;
-    String updateBy;
 }
