6588c1975750a98577c250cfb95f249736cda08a
Eledevo Technology <eledevo.git@gmail.com>
2024-09-20 02:59:38 +0000

    delete findByAccountsId in UsbRepository

--- src/main/java/vn/eledevo/vksbe/repository/UsbRepository.java
+++ src/main/java/vn/eledevo/vksbe/repository/UsbRepository.java
@@ -7,8 +7,5 @@ import org.springframework.data.jpa.repository.Query;
 import vn.eledevo.vksbe.entity.Usbs;
 
 public interface UsbRepository extends BaseRepository<Usbs, Long> {
-    @Query("SELECT u FROM Usbs u WHERE u.accounts.id = :accountId")
     Optional<Usbs> findByAccounts_Id(Long accountId);
-
-    Usbs findByAccountsId(Long accountId);
 }
