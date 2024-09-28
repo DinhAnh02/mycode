fe0ffc05ff79b3d162b422cb9ceb055ada2caa04
Quốc Tuấn Vương <tuanvq.eledevo@gmail.com>
2024-09-28 12:16:01 +0000

    Refactor/exception

---
+++ src/main/java/vn/eledevo/vksbe/constant/ErrorCodes/SystemErrorCode.java
@@ -0,0 +1,48 @@
+package vn.eledevo.vksbe.constant.ErrorCodes;
+
+import org.springframework.http.HttpStatusCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.http.HttpStatus.*;
+
+public enum SystemErrorCode implements BaseErrorCode {
+    INTERNAL_SERVER(INTERNAL_SERVER_ERROR, "500", "Internal Server Error", new HashMap<>()),
+    UNAUTHORIZED_SERVER(UNAUTHORIZED, "401", "Unauthorized", new HashMap<>()),
+    AUTHENTICATION_SERVER(FORBIDDEN, "403", "Forbidden", new HashMap<>()),
+    NOT_FOUND_SERVER(NOT_FOUND, "404", "Not Found", new HashMap<>()),
+    BAD_REQUEST_SERVER(BAD_REQUEST, "400", "Bad Request", new HashMap<>());
+
+    private final HttpStatusCode statusCode;
+    private final String code;  // Đảm bảo `code` là String
+    private final String message;
+    private final Map<String, String> result;  // Đảm bảo `result` là Map<String, String>
+
+    SystemErrorCode(HttpStatusCode statusCode, String code, String message, Map<String, String> result) {
+        this.statusCode = statusCode;
+        this.code = code;
+        this.message = message;
+        this.result = result;
+    }
+
+    @Override
+    public HttpStatusCode getStatusCode() {
+        return statusCode;
+    }
+
+    @Override
+    public String getCode() {  // Trả về kiểu String
+        return code;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public Map<String, String> getResult() {  // Đảm bảo `result` là Map<String, String>
+        return result;
+    }
+}
