abfb847ced8a8444fc00c1fb975a796cf08ded2a
Quốc Tuấn Vương <tuanvq.eledevo@gmail.com>
2024-09-29 07:29:13 +0000

    Refactor/get usb info

--- src/main/java/vn/eledevo/vksbe/dto/response/usb/UsbResponse.java
+++ src/main/java/vn/eledevo/vksbe/dto/response/usb/UsbResponse.java
@@ -1,12 +1,14 @@
 package vn.eledevo.vksbe.dto.response.usb;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
 @Getter
 @Setter
+@Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class UsbResponse {
     Long id;
