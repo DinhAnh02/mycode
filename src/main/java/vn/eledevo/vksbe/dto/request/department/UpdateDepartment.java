4d4a7a43892efd36fdeb788a9067ca862eee7094
Nguyen Tien Manh <manhnt.eledevo@gmail.com>
2024-10-09 16:16:05 +0700

    feat(department): add function UpdateDepartment in Service

---
+++ src/main/java/vn/eledevo/vksbe/dto/request/department/UpdateDepartment.java
@@ -0,0 +1,17 @@
+package vn.eledevo.vksbe.dto.request.department;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@Builder
+public class UpdateDepartment {
+    @NotBlank(message = "Không được để trống tên phòng ban")
+    @Size(min = 1, max = 50, message = "Tên phòng ban không được vượt quá 50 ký tự")
+    String departmentName;
+}
