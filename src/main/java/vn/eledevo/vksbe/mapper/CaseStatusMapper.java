6d9c3da66a92c665d83f1dcc08b075dfacc6d4c2
nguyennhatanh2k1 <nguyennhatanh2k1@gmail.com>
2024-10-10 09:19:03 +0000

    case status get

---
+++ src/main/java/vn/eledevo/vksbe/mapper/CaseStatusMapper.java
@@ -0,0 +1,47 @@
+package vn.eledevo.vksbe.mapper;
+
+import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+import vn.eledevo.vksbe.dto.response.account.AccountResponse;
+import vn.eledevo.vksbe.dto.response.case_status.CaseStatusResponse;
+import vn.eledevo.vksbe.dto.response.department.DepartmentResponse;
+import vn.eledevo.vksbe.entity.Accounts;
+import vn.eledevo.vksbe.entity.CaseStatus;
+import vn.eledevo.vksbe.entity.Departments;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+@Component
+public class CaseStatusMapper {
+    // Private constructor
+    private CaseStatusMapper(){};
+
+    public static CaseStatusResponse toResponse(CaseStatus e) {
+        if (Objects.isNull(e)) {
+            return new CaseStatusResponse();
+        }
+        return CaseStatusResponse.builder()
+                .id(e.getId())
+                .name(e.getName())
+                .description(e.getDescription())
+                .createdBy(e.getCreatedBy())
+                .updatedBy(e.getUpdatedBy())
+                .createdAt(e.getCreatedAt())
+                .updatedAt(e.getUpdatedAt())
+                .isDefault(false)
+                .build();
+    }
+    public List<CaseStatusResponse> toListResponse(List<CaseStatus> eList) {
+        if (CollectionUtils.isEmpty(eList)) {
+            return Collections.emptyList();
+        }
+
+        List<CaseStatusResponse> list = new ArrayList<>(eList.size());
+        for (CaseStatus caseStatus : eList) {
+            list.add(toResponse(caseStatus));
+        }
+        return list;
+    }
+}
---
+++ src/main/java/vn/eledevo/vksbe/service/case_status/CaseStatusService.java
@@ -0,0 +1,18 @@
+package vn.eledevo.vksbe.service.case_status;
+
+import vn.eledevo.vksbe.dto.request.CaseStatus.CaseStatusCreateRequest;
+import vn.eledevo.vksbe.dto.request.ComputerRequest;
+import vn.eledevo.vksbe.dto.request.case_status.CaseStatusGetRequest;
+import vn.eledevo.vksbe.dto.response.ResponseFilter;
+import vn.eledevo.vksbe.dto.response.case_status.CaseStatusResponse;
+import vn.eledevo.vksbe.dto.response.computer.ComputerResponseFilter;
+import vn.eledevo.vksbe.exception.ApiException;
+
+import java.util.HashMap;
+
+public interface CaseStatusService {
+    ResponseFilter<CaseStatusResponse> getCaseStatus(
+            CaseStatusGetRequest caseStatusGetRequest,Integer page, Integer pageSize) throws ApiException;
+
+    HashMap<String,String> createCaseStatus(CaseStatusCreateRequest caseStatusCreateRequest) throws  ApiException;
+}
